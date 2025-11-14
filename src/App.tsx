c987d54ade032bd51a271b8413fd7115d204bb64
hung19091 <88091495+hung19091@users.noreply.github.com>
2025-11-14 11:46:57 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import "./styles.css";
+import { useState } from "react";
 
-export default function App() {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   );
 }
+
+export default App;
