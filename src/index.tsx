325eeb1c12a089731d5f42ed43e83a8b71d82b7a
hung19091 <88091495+hung19091@users.noreply.github.com>
2025-11-14 11:49:03 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>React Demo</title>
-</head>
-<body>
-  <div id="root"></div>
-</body>
-</html>
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
